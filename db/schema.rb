5bd9a95bd3940a471f0ec154ff5d3c219ede4a75
patw0929 <patw@techbang.com.tw>
2013-09-11 11:47:24 +0800

    依照 has_many 與 belongs_to 一節增加 post 的 board_id 欄位.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130910090238) do
+ActiveRecord::Schema.define(:version => 20130911034606) do
 
   create_table "boards", :force => true do |t|
     t.string   "name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(:version => 20130910090238) do
     t.text     "content"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "board_id"
   end
 
 end
