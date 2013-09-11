6ffc109dc20709ea5851014f0f432f2245daafbf
patw0929 <patw@techbang.com.tw>
2013-09-11 09:31:23 +0800

    新增 board new 功能.

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -9,4 +9,20 @@ class BoardsController < ApplicationController
   def show
   end
 
+  # 新增
+  def new
+    @board = Board.new
+  end
+
+  # 新增 (寫入)
+  def create
+    @board = Board.new(params[:board])
+
+    if @board.save
+      redirect_to boards_path
+    else
+      render :action => "new"
+    end 
+  end
+
 end
