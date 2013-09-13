9b09154719be967abaa58cb5f9ca3f58d400d94d
patw0929 <patw@techbang.com.tw>
2013-09-13 13:14:18 +0800

    附件上傳 - 增加資料表欄位與相關設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,5 @@ class Post < ActiveRecord::Base
   belongs_to :board, :counter_cache => true
   belongs_to :user
   scope :recent, :order => "updated_at DESC"
+  has_attached_file :excerpt_image, :styles => { :show => "640x480>", :thumb => "100x100#" }
 end
