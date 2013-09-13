91ea986c89ec72288653ed72f62be7506e00f56e
patw0929 <patw@techbang.com.tw>
2013-09-13 11:14:44 +0800

    將 migration 中的 up & down 合併成 change 的寫法

--- db/migrate/20130911034606_add_board_id_to_post.rb
+++ db/migrate/20130911034606_add_board_id_to_post.rb
@@ -1,9 +1,12 @@
 class AddBoardIdToPost < ActiveRecord::Migration
-  def self.up
+  def change
     add_column :posts, :board_id, :integer
   end
 
-  def self.down
-    remove_column :posts, :board_id
-  end
+  # NOTE: 原本是寫 self.up 跟 self.down，但在 Rails 3 之後可以用 change 方法來取代，
+  # 會自動做相應的動作。
+  #
+  # def self.down
+  #   remove_column :posts, :board_id
+  # end
 end
--- db/migrate/20130912022348_add_user_id_to_post.rb
+++ db/migrate/20130912022348_add_user_id_to_post.rb
@@ -1,9 +1,5 @@
 class AddUserIdToPost < ActiveRecord::Migration
-  def self.up
+  def change
     add_column :posts, :user_id, :integer
-  end
-
-  def self.down
-    remove_column :posts, :user_id
   end
 end
