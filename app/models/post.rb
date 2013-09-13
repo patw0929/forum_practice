e3b9cc331e988f90b6aad5b08b8d144e7488c1f6
patw0929 <patw@techbang.com.tw>
2013-09-13 13:55:34 +0800

    將 excerpt_image 加入白名單

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  attr_accessible :content, :title
+  attr_accessible :content, :title, :excerpt_image
   belongs_to :board, :counter_cache => true
   belongs_to :user
   scope :recent, :order => "updated_at DESC"
