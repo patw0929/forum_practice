1a17da80908da09780ddc8aeca1c7feac08a75d1
Patrick Wang <patw.hi@gmail.com>
2013-09-18 14:38:21 +0800

    deploy:restart 追加的 hook 應擇一使用，這次的票要求是 unicorn:reload

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,5 +7,3 @@ set :default_stage, "production"
 # set :normalize_asset_timestamps, false
 
 after 'deploy:restart', 'unicorn:reload'    # app IS NOT preloaded
-after 'deploy:restart', 'unicorn:restart'   # app preloaded
-after 'deploy:restart', 'unicorn:duplicate' # before_fork hook implemented (zero downtime deployments)
