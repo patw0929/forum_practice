ecf3103983a2441bc9ea66f48c482f6e7ffcdd8b
Patrick Wang <patw.hi@gmail.com>
2013-09-18 11:54:46 +0800

    修改 unicorn 的 worker_processes 為 1

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -7,7 +7,7 @@
 # a more verbose configuration using more features.
 
 listen 2007 # by default Unicorn listens on port 8080
-worker_processes 2 # this should be >= nr_cpus
+worker_processes 1 # this should be >= nr_cpus
 pid "/home/apps/forum_practice/shared/pids/unicorn.pid"
 stderr_path "/home/apps/forum_practice/shared/log/unicorn.log"
 stdout_path "/home/apps/forum_practice/shared/log/unicorn.log"
